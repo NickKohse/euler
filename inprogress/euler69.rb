9c492986e6a2c600279ee8739fcb105bef2961ad
Nick Kohse <nick.kohse@gmail.com>
2022-11-27 15:08:56 -0700

    Speedup

--- inprogress/euler69.rb
+++ inprogress/euler69.rb
@@ -1,9 +1,13 @@
+$max = 0
+
 def relativley_prime(p, n) # i.e. gcd(p,n) = 1, use euclidean gcd
+
     while p != 0
         x = n % p
         n = p
         p = x
     end
+
     return n == 1
 end
 
@@ -13,19 +17,21 @@ def phi(n) # return num of nums less than n which are relativley prime to n
         if relativley_prime(i, n)
             count += 1
         end
+        if n / count.to_f < $max
+            return count # short circuit if we are alreday under max, cuts time in half for n = 10K
+        end
     end
     return count
 end
 
-max = 0
 max_i = 0
 
 for i in 1..1_000_000
     quotient = i / phi(i).to_f
     puts i if i % 1000 == 0
-    if quotient > max
+    if quotient > $max
         max_i = i
-        max = quotient
+        $max = quotient
     end
 end
 
